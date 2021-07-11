b710ea1a73f8cb0216a5d1a856c4f781987507bf
nidoos <hsoobin21@gmail.com>
2021-07-11 23:19:13 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,31 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OderServiceImpl;
 import hello.core.order.OrderService;
 
+//애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악가능
+//역할을 세우고 구현이 그 안에 들어가도록. 중복제거.
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
